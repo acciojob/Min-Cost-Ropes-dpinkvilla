99912ca4a3d94a8495109cac2576588c66733b33
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-02-21 11:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	arr.sort(function (a,b) {return a-b});
+
+	totalCost=0;
+
+	while (arr.length >= 2) {
+
+		let rope1 = arr.shift();
+		let rope2 = arr.shift();
+
+		let combinedLength = rope1+rope2;
+
+		totalCost + = combinedLength;
+
+		arr.push(combinedLength);
+       arr.sort(function (a,b) {return a-b});
+		
+	}
+		
+	return totalCost;
   
 }
 
